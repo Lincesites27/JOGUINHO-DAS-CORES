5b80f4e972600c029e64a39ce32b03e4ab644fd7
lincesites27 <contato@lincesites.com.br>
2022-11-22 14:29:48 -0300

    contadores

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const main = document.querySelector('#main');
+var contadorAzul = 0 
+var contadorAmarelo = 0 
+var contadorVerde = 0 
+var contadorRosa = 0 
 
-const trocaClasseRosa = ()=>{
-    document.getElementById('main').classList.toggle('rosa')
-    document.getElementById('main').classList.remove('verde', 'azul', 'amarelo')
-   
+
+
+const trocaClasse = (cor)=>{
+   const main = document.querySelector('#main');
+   main.className= "main";
+   main.classList.add(cor);
+//    main.classList.add("")
+
+    if(cor === 'azul'){
+        contadorAzul++;
+        document.querySelector("#contadorAzul").innerHTML='Azul: ' +contadorAzul;
+    }
+
+    if(cor === 'verde'){
+        contadorVerde++;
+        document.querySelector("#contadorVerde").innerHTML='Verde: ' +contadorVerde;
+    }
+
+    if(cor === 'amarelo'){
+        contadorAmarelo++;
+        document.querySelector("#contadorAmarelo").innerHTML='Amarelo: ' +contadorAmarelo;
+    }
+
+    if(cor === 'rosa'){
+        contadorRosa++;
+        document.querySelector("#contadorRosa").innerHTML='Rosa: ' +contadorRosa;
+    }
+    
+    
+    
+
 }
-const trocaClasseVerde = ()=>{
-    document.getElementById('main').classList.toggle('verde')
-    document.getElementById('main').classList.remove('azul', 'rosa', 'amarelo')
- 
+
+const zerarContador = ()=>{
+    contadorAzul = 0 
+    contadorAmarelo = 0 
+    contadorVerde = 0 
+    contadorRosa = 0 
+    
+    document.querySelector("#contadorAmarelo").innerHTML='Amarelo: ' +contadorAmarelo;
+    document.querySelector("#contadorRosa").innerHTML='Rosa: ' +contadorRosa;
+    document.querySelector("#contadorVerde").innerHTML='Verde: ' +contadorVerde;
+    document.querySelector("#contadorAzul").innerHTML='Azul: ' +contadorAzul;
 }
-const trocaClasseAzul = ()=>{
-    document.getElementById('main').classList.toggle('azul')
-    document.getElementById('main').classList.remove('verde', 'rosa', 'amarelo')
-   
-}
-const trocaClasseAmarelo = ()=>{
-    document.getElementById('main').classList.toggle('amarelo')
-    document.getElementById('main').classList.remove('verde', 'azul', 'rosa')
-   
-}
+
